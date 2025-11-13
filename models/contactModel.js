f73e656ff6b7c1474976bf546afb23998380c267
kartik chaudhary <kartikchaudharih18@gmail.com>
2025-11-13 14:40:10 +0530

    first commit1

--- models/contactModel.js
+++ models/contactModel.js
@@ -2,7 +2,7 @@ import pool from "../config/db.js";
 
 export const saveContact = async (name, email, message) => {
   const result = await pool.query(
-    "INSERT INTO contact (name, email, message) VALUES ($1, $2, $3) RETURNING *",
+    "INSERT INTO contacts (name, email, message) VALUES ($1, $2, $3) RETURNING *",
     [name, email, message]
   );
   return result.rows[0];
